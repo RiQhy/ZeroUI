c4eaec6befa0df33e30f5b603ee2197e81a7f047
Emil-Zghaib <emilz@metropolia.fi>
2024-04-12 15:19:42 +0300

    kasvojen ja silmien tunnistus
    eteenpäin

---
+++ app/src/main/java/com/example/zeroui/CameraPermissionHandler.kt
@@ -0,0 +1,39 @@
+package com.example.zeroui
+
+import android.Manifest
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.shouldShowRationale
+
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraPermissionHandler() {
+
+    val cameraPermissionState = rememberPermissionState(
+        Manifest.permission.CAMERA
+    )
+
+    if (cameraPermissionState.status.isGranted) {
+        Text("Camera permission Granted")
+    } else {
+        Column {
+            val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
+                "The camera is important for this app. Please grant the permission."
+            } else {
+                "Camera permission required for this feature to be available. " +
+                        "Please grant the permission"
+            }
+            Text(textToShow)
+            Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
+                Text("Request permission")
+            }
+        }
+    }
+}
+
