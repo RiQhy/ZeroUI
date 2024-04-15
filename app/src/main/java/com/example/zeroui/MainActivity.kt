f476294f36a68b75553ca76385b8e881143f3d2d
RiQhy <riku.nokelainen@metropolia.fi>
2024-04-15 20:07:54 +0300

    Rotation vector
    Isnt finished yet

--- app/src/main/java/com/example/zeroui/MainActivity.kt
+++ app/src/main/java/com/example/zeroui/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.example.zeroui
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,14 +21,46 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.zeroui.ui.theme.ZeroUITheme
 
 class MainActivity : ComponentActivity() {
+    val viewModel: ViewModel by viewModels()
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+        if (!isGranted) {
+            // Inform the user that the permission is needed.
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val permissionsGranted = HashMap<String, Boolean>()
+        val requiredPermissions: Array<String> = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch(Manifest.permission.ACTIVITY_RECOGNITION)
+        }
+        val requestPermissionsLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                permissions.entries.forEach { entry ->
+                    if (entry.value) {
+                        permissionsGranted[entry.key] = entry.value
+                        Log.d("PermissionGranted", "Permission ${entry.key} is granted")
+                    } else {
+                        Log.d("PermissionDenied", "Permission ${entry.key} is denied")
+                    }
+                }
+            }
+
         setContent {
             ZeroUITheme {
                 // A surface container using the 'background' color from the theme
@@ -29,6 +70,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Navigation()
                 }
+                requestPermissionsLauncher.launch(requiredPermissions)
             }
         }
     }
