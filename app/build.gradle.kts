252c580560e44d405809811eeed548851a844315
Christian <christian.olkkonen@metropolia.fi>
2024-04-25 14:32:11 +0300

    working now
    on 0.10  not 0.20+

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,9 +69,6 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.navigation:navigation-fragment:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
@@ -100,7 +97,7 @@ dependencies {
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
 
-    // Mediapipe Library
-    implementation ("com.google.mediapipe:tasks-vision:0.20230731")
+    // Mediapipe Library must use 0.10 to work
+    implementation ("com.google.mediapipe:tasks-vision:0.10.0")
     implementation ("com.google.accompanist:accompanist-permissions:0.23.1")
 }
