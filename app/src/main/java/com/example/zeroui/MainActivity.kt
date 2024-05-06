7e42822967cabd56474df22130e18ddeb0910d4c
Semen Morozov <semenmorozovsuomi@gmail.com>
2024-05-06 17:08:08 +0300

    #MainActivity changes permission requests

--- app/src/main/java/com/example/zeroui/MainActivity.kt
+++ app/src/main/java/com/example/zeroui/MainActivity.kt
@@ -1,28 +1,22 @@
 package com.example.zeroui
 
 import android.Manifest
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.ViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -35,83 +29,99 @@ class MainActivity : ComponentActivity() {
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
             if (!isGranted) {
                 // Inform the user that the permission is needed.
+            }
         }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val permissionsGranted = HashMap<String, Boolean>()
+        // Define the required permissions
         val requiredPermissions: Array<String> = arrayOf(
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.RECORD_AUDIO,
             Manifest.permission.ACTIVITY_RECOGNITION
-
         )
-        if (ContextCompat.checkSelfPermission(
-                this, Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermissionLauncher.launch(Manifest.permission.ACTIVITY_RECOGNITION)
-        }
+
+        // Register for permission request result
         val requestPermissionsLauncher =
             registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
                 permissions.entries.forEach { entry ->
                     if (entry.value) {
-                        permissionsGranted[entry.key] = entry.value
                         Log.d("PermissionGranted", "Permission ${entry.key} is granted")
                     } else {
                         Log.d("PermissionDenied", "Permission ${entry.key} is denied")
+                        // You can handle the scenario where a permission is denied here
+                    }
+                }
+                // Continue with your app's main logic
+                setContent {
+                    ZeroUITheme {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            Navigation(this@MainActivity)
+                        }
                     }
                 }
             }
 
-        setContent {
-            ZeroUITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Navigation(this@MainActivity)
+        // Check if permissions are missing, and if so, request them
+        val missingPermissions = requiredPermissions.filter {
+            ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+        }
+        if (missingPermissions.isNotEmpty()) {
+            // Request missing permissions
+            requestPermissionsLauncher.launch(missingPermissions.toTypedArray())
+        } else {
+            // If all required permissions are already granted, proceed with the app's main logic
+            setContent {
+                ZeroUITheme {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        Navigation(this@MainActivity)
+                    }
                 }
-                requestPermissionsLauncher.launch(requiredPermissions)
             }
         }
     }
-}
 
-@OptIn(ExperimentalAnimationApi::class)
-@Composable
-fun Navigation(context: Context) {
-    val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "Front View") {
-        composable("Front View") {
+
+    @OptIn(ExperimentalAnimationApi::class)
+    @Composable
+    fun Navigation(context: Context) {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "Front View") {
+            composable("Front View") {
+                FrontViewPage(
+                    onGazeClick = { navController.navigate("CameraScreen") },
+                    onGestureClick = { fragmentActivity(context) },
+                    onMotionClick = { navController.navigate("Motion Sensors") },
+                )
+            }
+            composable("Motion Sensors") { MotionSensorView(navController) }
+            composable("CameraScreen") { CameraScreenView(navController) }
+            //composable("") {  }
+        }
+    }
+
+    fun fragmentActivity(context: Context) {
+        val intent = Intent(context, MainActivity2::class.java)
+        context.startActivity(intent)
+    }
+
+    @OptIn(ExperimentalAnimationApi::class)
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        ZeroUITheme {
             FrontViewPage(
-                onGazeClick = { navController.navigate("CameraScreen") },
-                onGestureClick = { fragmentActivity(context) },
-                onMotionClick = { navController.navigate("Motion Sensors") },
+                onGazeClick = {},
+                onGestureClick = {},
+                onMotionClick = {},
             )
         }
-        composable("Motion Sensors") { MotionSensorView(navController) }
-        composable("CameraScreen") { CameraScreenView(navController) }
-        //composable("") {  }
-    }
-}
-
-fun fragmentActivity(context: Context) {
-    val intent = Intent(context, MainActivity2::class.java)
-    context.startActivity(intent)
-}
-
-@OptIn(ExperimentalAnimationApi::class)
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ZeroUITheme {
-        FrontViewPage(
-            onGazeClick = {},
-            onGestureClick = {},
-            onMotionClick = {},
-        )
     }
 }
